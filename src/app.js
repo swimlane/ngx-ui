a92a5d2372a75c31af20817e16c2ccb2e3671178
Austin McDaniel <amcdaniel2@gmail.com>
2016-08-18 15:48:43 -0400

    all color pallets applied and typography

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ import './assets/fonts/icons/icons-font.js';
         <nav>
           <ul>
             <li><a href="#">Colors</a></li>
-            <li><a href="#">Text</a></li>
+            <li><a href="#">Typography</a></li>
             <li><a href="#">Buttons</a></li>
           </ul>
         </nav>
@@ -29,7 +29,6 @@ import './assets/fonts/icons/icons-font.js';
             Style Guide
             <small>v{{version}}</small>
           </h2>
-
           <div class="u-floatRight">
             <a href="#">Light</a> | <a href="#">Dark</a>
           </div>
@@ -38,40 +37,37 @@ import './assets/fonts/icons/icons-font.js';
         <section>
           <h3 class="styleHeader">Colors</h3>
 
-          <div class="Grid Grid--withGutter Grid--fit">
-
-            <div class="Grid-cell">
-              <ul class="color-group">
-                <li class="color main-color u-cf color-blue">
-                  <span class="name u-floatLeft">Blues</span>
-                  <span class="hex tag tag-small u-floatRight">--color-blue</span>
+          <div class="u-flex u-flexWrap">
+            <div class="FlexItem" *ngFor="let color of colors">
+              <ul class="color-group shadow-2 shadow-fx">
+                <li class="color main-color u-cf color-{{color}}">
+                  <span class="name u-floatLeft">{{color}}</span>
+                  <span class="hex tag tag-small u-floatRight">--color-{{color}}</span>
                 </li>
-                <li class="color color-blue-med u-cf">
-                  <span class="hex tag tag-small u-floatRight">--color-blue-med</span>
+                <li class="color color-{{color}}-med u-cf">
+                  <span class="hex tag tag-small u-floatRight">--color-{{color}}-med</span>
                 </li>
-                <li class="color color-blue-light u-cf">
-                  <span class="hex tag tag-small u-floatRight">--color-blue-light</span>
-                </li>
-              </ul>
-            </div>
-
-            <div class="Grid-cell">
-              <ul class="color-group">
-                <li class="color main-color u-cf color-green">
-                  <span class="name u-floatLeft">Greens</span>
-                  <span class="hex tag tag-small u-floatRight">--color-green</span>
-                </li>
-                <li class="color color-green-med u-cf">
-                  <span class="hex tag tag-small u-floatRight">--color-green-med</span>
-                </li>
-                <li class="color color-green-light u-cf">
-                  <span class="hex tag tag-small u-floatRight">--color-green-light</span>
+                <li class="color color-{{color}}-light u-cf">
+                  <span class="hex tag tag-small u-floatRight">--color-{{color}}-light</span>
                 </li>
               </ul>
             </div>
-
           </div>
+        </section>
 
+        <section>
+          <h3 class="styleHeader">Typography: Headings</h3>
+          <h1>h1. Improve your Security Operations</h1>
+          <h2>h2. Improve your Security Operations</h2>
+          <h3>h3. Improve your Security Operations</h3>
+          <h4>h4. Improve your Security Operations</h4>
+          <h5>h5. Improve your Security Operations</h5>
+        </section>
+
+        <section>
+          <h3 class="styleHeader">Typography: Paragraph</h3>
+          <p>As cyber attacks continue to rise, organizations are investing heavily in attack identification, threat intelligence and the staff required to protect the enterprise. However, alerts are still going unresolved, and often unseen. Realizing that simply adding people does not solve the problem, organizations are choosing Swimlane for security automation and orchestration</p>
+          <p>Swimlane consolidates security alerts from multiple sources and automatically assists organizations with the activities required to resolve alerts and stop attacks. The resolution of the alert can occur either automatically or manually by analyst intervention. Either way, the alert is resolved utilizing expert-defined processes, enabling the organization to cost-effectively close alerts.</p>
         </section>
 
         <section>
@@ -89,9 +85,11 @@ export class App {
   version = APP_VERSION;
 
   colors = [
-
-    [ '' ]
-
-  ]
+    'blue',
+    'light-blue',
+    'green',
+    'red',
+    'orange'
+  ];
 
 }
