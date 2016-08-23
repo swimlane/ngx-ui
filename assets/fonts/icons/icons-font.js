684f36bfb0ef7c4b7f5387d041f391af914e5149
Austin McDaniel <amcdaniel2@gmail.com>
2016-08-23 09:22:40 -0400

    Updates

---
+++ assets/fonts/icons/icons-font.js
@@ -0,0 +1,13 @@
+module.exports = {
+  fontName : "icon",
+  files : [
+    "../../svgs/*.svg"
+  ],
+  baseClass : "icon",
+  classPrefix : "icon-",
+  normalize: true,
+  fontHeight: 1001,
+  fixedWidth: true,
+  cssTemplate: './template.css',
+  types: ['ttf', 'eot', 'woff', 'svg']
+};
