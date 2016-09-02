88baf21edd3c3ea1a6cfa2d09fbb205ee2c24b55
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-02 11:41:33 -0400

    (bug): fix issue missing ngFor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 // components
 import { DblClickCopy } from './directives/DblClickCopy.js';
 import { Toolbar } from './components/toolbar/Toolbar.js';
 import { DropdownMenu, DropdownToggle, Dropdown } from './components/dropdown/Dropdown.js';
 import { CodeHighlight } from './components/codeHighlight/CodeHighlight.js';
+import { Drawer, DrawerManager, DrawerContainer, DrawerOverlay } from './components/drawer/DrawerModule.js';
 
-import {
-  Drawer,
-  DrawerManager,
-  DrawerContainer,
-  DrawerOverlay
-} from './components/drawer/DrawerModule.js';
-
+// styles
 import './styles/index.scss';
 
+// all components
 export const components = [
   DblClickCopy,
   Toolbar,
@@ -29,13 +26,16 @@ export const components = [
   DropdownToggle
 ];
 
+// all providers
 export const providers = [
   DrawerManager
 ];
 
+// module
 @NgModule({
   declarations: components,
   exports: components,
-  providers: providers
+  providers: providers,
+  imports: [BrowserModule]
 })
 export class CommonModule { }
