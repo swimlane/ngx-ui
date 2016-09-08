9ba8aa3d2f4e18a206f415acc249bbd80d6ae72c
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-08 11:06:33 -0400

    (component): password complexity and matching

--- src/components/password/PasswordStrength.js
+++ src/components/password/PasswordStrength.js
@@ -1,4 +1,6 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostBinding } from '@angular/core';
+
+import zxcvbn from 'zxcvbn';
 import './passwordStrength.scss';
 
 let nextId = 0;
@@ -11,9 +13,10 @@ let nextId = 0;
         <input
           type="password"
           [hidden]="textVisible"
-          class="password-strength-input password-strength-password"
+          class="password-strength-input password-strength-password full-width"
           [id]="id + '-pass'"
           [name]="name"
+          [placeholder]="placeholder"
           [value]="value"
           (keyup)="onKeyUp($event)"
           required
@@ -21,9 +24,10 @@ let nextId = 0;
         <input
           type="text"
           [hidden]="!textVisible"
-          class="password-strength-input password-strength-txt"
+          class="password-strength-input password-strength-txt full-width"
           [id]="id + '-txt'"
           [name]="name"
+          [placeholder]="placeholder"
           [value]="value"
           (keyup)="onKeyUp($event)"
           required
@@ -36,38 +40,71 @@ let nextId = 0;
       </div>
       <meter
         max="4"
-        [value]="strength"
+        [value]="score"
+        [class.has-value]="value.length"
         class="password-strength-meter">
-        <div class="meter-text">{{message}}</div>
+        <div
+          class="meter-text"
+          [hidden]="!showMessage"
+          [innerHTML]="message">
+        </div>
       </meter>
     </div>
-  `,
-  host: {
-    class: 'password-strength'
-  }
+  `
 })
 export class PasswordStrength {
 
   @Input() id = `pstrength-${++nextId}`;
   @Input() name = null;
   @Input() value = '';
-
-  @Input() minLength = 10;
-  @Input() maxLength = 120;
-
-  @Input() requireUppercase = true;
-  @Input() requireLowercase = true;
-  @Input() requireNumber = true;
-  @Input() requireSpecialChars = true;
+  @Input() placeholder = '';
+  @Input() showMessage = true;
+  @Input() showAscent = true;
 
   @Output() onChange = new EventEmitter();
 
-  get strength() {
-    return 0;
+  @HostBinding('class')
+  get cssClasses() {
+    let clz = 'password-strength';
+
+    if(this.value.length) {
+      clz += ' has-value';
+    }
+
+    if(this.showAscent) {
+      clz += ' ascent-score';
+    }
+
+    clz += ` score-${this.score}`;
+
+    return clz;
+  }
+
+  get score() {
+    if(!this.results) return 0;
+    return this.results.score;
   }
 
   get message() {
-    return 'Complex passwords are required';
+    if(!this.results || !this.value.length)
+      return 'Strong passwords are required';
+
+    let msg;
+    if(this.score === 0 || this.score === 1) {
+      msg = 'Weak Password'
+    } else if(this.score === 2 || this.score === 3) {
+      msg = 'Average Password'
+    } else if(this.score === 4) {
+      msg = 'Strong Password'
+    }
+
+    msg = `<strong>${msg}</strong>`;
+
+    if(this.results.feedback.warning) {
+      msg = `${msg} | ${this.results.feedback.warning}`;
+    }
+
+    return msg;
   }
 
   ngOnInit() {
@@ -78,12 +115,11 @@ export class PasswordStrength {
   onKeyUp(event) {
     const value = event.target.value;
     this.value = value;
-
-    const results = {};
+    this.results = zxcvbn(value);
 
     this.onChange.emit({
       value,
-      results
+      results: this.results
     });
   }
 
