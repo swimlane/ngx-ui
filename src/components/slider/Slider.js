9b49871292727848356849698ada07a17b762714
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-12 16:06:15 -0400

    (component): slider DONE!

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,12 +1,17 @@
 import { Component, Input, Output, EventEmitter, HostListener, HostBinding } from "@angular/core";
 import './slider.scss';
 
+let nextId = 0;
+
 @Component({
   selector: 'slider',
   template: `
-    <div>
+    <div class="slider-inner">
       <input
         type="range"
+        [id]="id"
+        [attr.list]="id + '-list'"
+        [attr.orientation]="orientation"
         [(ngModel)]="value"
         [min]="min"
         [max]="max"
@@ -15,6 +20,19 @@ import './slider.scss';
         (input)="changed($event)"
         (change)="changed($event)"
       />
+      <span
+        *ngIf="filled"
+        [ngStyle]="getFill()"
+        class="fill-bar">
+      </span>
+      <datalist
+        *ngIf="showTicks"
+        [id]="id + '-list'">
+      	<option
+          *ngFor="let i of count">
+          {{i}}
+        </option>
+      </datalist>
     </div>
   `,
   host: {
@@ -23,11 +41,21 @@ import './slider.scss';
 })
 export class Slider {
 
+  @Input() id = `range-${++nextId}`;
   @Input() min = 0;
   @Input() max = 100;
   @Input() step = 1;
+  @Input() orientation = 'horizontal';
+  @Input() filled = false;
+
+  // Not supported in all
+  // browers see polyfill
+  // http://leaverou.github.io/multirange/
   @Input() multiple = false;
-  @Input() direction = 'horizontal';
+
+  @Input() showTicks = false;
+  @Input() tickStep;
+  count = [];
 
   @Input()
   set value(val) {
@@ -40,16 +68,46 @@ export class Slider {
     return this._value.join(',');
   }
 
+  getCount() {
+    let idxs = [];
+    const step = this.tickStep || this.step;
+
+    let i = this.min;
+    while(i <= this.max) {
+      idxs.push(i);
+      i+= step;
+    }
+
+    return idxs;
+  }
+
+  getFill() {
+    if(this.filled) {
+      const size = this.isHorizontal ?
+        `${this.percent}% 100%` :
+        `100% ${this.percent}%`;
+
+      return {
+        'background-size': size
+      };
+    }
+  }
+
   @Output() onChange = new EventEmitter();
+
+  @HostBinding('class.filled')
+  get isFilled() {
+    return this.filled;
+  }
 
   @HostBinding('class.horizontal')
   get isHorizontal() {
-    return this.direction === 'horizontal';
+    return this.orientation === 'horizontal';
   }
 
   @HostBinding('class.vertical')
   get isVertical() {
-    return this.direction === 'vertical';
+    return this.orientation === 'vertical';
   }
 
   @HostBinding('class.active')
@@ -69,6 +127,12 @@ export class Slider {
   @HostListener('mouseup', ['$event'])
   onMouseUp() {
     this.active = false;
+  }
+
+  ngOnInit() {
+    if(this.showTicks) {
+      this.count = this.getCount();
+    }
   }
 
   changed(event) {
