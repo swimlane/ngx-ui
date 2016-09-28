d12a0413d6bdbc60d5e08c0f9a4c79a5de6a775b
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-28 17:13:08 -0400

    (styles): normalized css for ul/li and added reset, added class modifier for inputs and btns, added subtitle ascenting for headers, scaffold of calendar

--- src/components/calendar/calendar.module.ts
+++ src/components/calendar/calendar.module.ts
@@ -1,1 +1,12 @@
-export class CalendarModule {}
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { CalendarComponent } from './calendar.component';
+
+@NgModule({
+  declarations: [CalendarComponent],
+  exports: [CalendarComponent],
+  imports: [BrowserModule, FormsModule]
+})
+export class CalendarModule { }
