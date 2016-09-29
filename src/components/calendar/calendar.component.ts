5f05c3e32224a3213086758983a423a67c1fec63
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-29 16:35:29 -0400

    (style): adding table base styles, add table example, add native font stack, mutuing calendar code violation

--- src/components/calendar/calendar.component.ts
+++ src/components/calendar/calendar.component.ts
@@ -3,10 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'swui-calendar',
   template: `
-  <h1>hi</h1>
+    <h1>hi</h1>
   `
 })
 export class CalendarComponent {
-
 
 }
