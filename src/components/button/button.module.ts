70c611870545f6bb58cbe250056cb93130ab5243
Austin McDaniel <amcdaniel2@gmail.com>
2016-10-26 10:02:20 -0400

    (chore): removing unused import for button

--- src/components/button/button.module.ts
+++ src/components/button/button.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { FileUploadModule } from 'ng2-file-upload';
 import { FileButtonComponent } from './file-button.component';
