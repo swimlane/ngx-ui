3c4b0c9a1600a9043c9be1b18365952c4e6d87a2
Austin McDaniel <amcdaniel2@gmail.com>
2016-11-03 12:44:16 -0400

    (deploy): CI

--- demo/lib.d.ts
+++ demo/lib.d.ts
@@ -1,3 +1,8 @@
+import 'core-js/es6';
+import 'core-js/es7/object';
+import 'core-js/es7/reflect';
+import 'ts-helpers';
+import 'zone.js/dist/zone';
 import 'rxjs';
 import '@angular/platform-browser-dynamic';
 import '@angular/platform-browser';
