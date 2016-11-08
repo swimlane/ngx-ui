d723e1b99aa96b881b6d963976e29861987f07a0
Austin McDaniel <amcdaniel2@gmail.com>
2016-11-08 17:38:46 +0000

    (deploy): CI

--- src/services/registry.service.d.ts
+++ src/services/registry.service.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="core-js" />
 export declare class RegistryService {
     components: Map<{}, {}>;
-    register(id: string, component: any, callback?: any): void;
+    register(id: string, component: any, destroyCallback?: any): void;
     get(id: string): any;
     destroy(id: string): void;
     destroyAll(): void;
