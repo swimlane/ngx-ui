319141b92d43ac3d14032d69623615131a92e6dd
Marjan Georgiev <marjan.georgiev@gmail.com>
2017-04-03 23:51:59 +0200

    feat(ngx-button) Add ngx-button component

--- src/components/button/button.module.ts
+++ src/components/button/button.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { FileUploadModule } from 'ng2-file-upload';
 import { FileButtonComponent } from './file-button.component';
+import { ButtonComponent } from './button.component';
 
 @NgModule({
-  declarations: [FileButtonComponent],
-  exports: [FileButtonComponent, FileUploadModule],
+  declarations: [FileButtonComponent, ButtonComponent],
+  exports: [FileButtonComponent, FileUploadModule, ButtonComponent],
   imports: [CommonModule, FileUploadModule]
 })
 export class ButtonModule { }
