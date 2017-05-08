f07fc1612d2c66c2283d90484c266cbedaccb1db
Jayson Harshbarger <hypercubed@gmail.com>
2017-05-08 16:07:03 -0600

    Update link color and show hex code in demo

--- config/colors.js
+++ config/colors.js
@@ -4,10 +4,9 @@ const { writeFileSync } = require('fs');
 
 const joinPath = path.join.bind(path, path.resolve(__dirname, '../src/styles/colors/'));
 
-const hues = sync({ input: joinPath('hues.scss') });
 const branding = sync({ input: joinPath('branding.scss') });
-const element = sync({ input: joinPath('element.scss') });
 
-writeFileSync(joinPath('colors.json'), JSON.stringify(hues, null, ' '));
 writeFileSync(joinPath('branding.json'), JSON.stringify(branding, null, ' '));
-writeFileSync(joinPath('element.json'), JSON.stringify(element, null, ' '));
+
+
+
