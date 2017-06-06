66e2ca686b997d8d1f9d0cc7552998f718db09cb
Marjan Georgiev <marjan.georgiev@gmail.com>
2017-06-07 00:36:24 +0200

    Bug: Export service and components from hotkeys module. Fix sorting in hotkeys combination

--- src/components/hotkeys/hotkeys.module.ts
+++ src/components/hotkeys/hotkeys.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HotkeysComponent } from './hotkeys.component';
 import { HotkeysService } from './hotkeys.service';
 
+export * from './hotkeys.service';
+export * from './hotkeys.component';
+
 @NgModule({
   declarations: [HotkeysComponent],
   exports: [HotkeysComponent],
