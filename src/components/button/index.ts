5e324fe1ea2b2b71a1940f532524d37834aae834
Jayson Harshbarger <hypercubed@gmail.com>
2018-02-13 14:05:18 -0700

    Add all components to exports

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,2 +1,3 @@
 export * from './button.module';
 export * from './file-button.component';
+export * from './button.component';
--- src/components/long-press/index.ts
+++ src/components/long-press/index.ts
@@ -1,1 +1,2 @@
 export * from './long-press-button.module';
+export * from  './long-press-button.component';
