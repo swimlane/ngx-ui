58fe2763a01c64987cc2eb7756a9c0d6ca2a3779
Swimlane <developer@swimlane.com>
2018-12-10 20:04:14 +0100

    (deploy): CI

--- polyfills.js
+++ polyfills.js
@@ -5497,8 +5497,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/marjan/Projects/ngx-ui/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /home/marjan/Projects/ngx-ui/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/marjan/Projects/ngx-ui/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/marjan/Projects/ngx-ui/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
