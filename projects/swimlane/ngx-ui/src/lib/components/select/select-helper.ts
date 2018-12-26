08e138f323476afbc02e9a94fbad7197efe2ccb2
Jayson Harshbarger <hypercubed@gmail.com>
2018-12-26 13:45:34 -0700

    added [caseSensitive] input to ngx-select

--- projects/swimlane/ngx-ui/src/lib/components/select/select-helper.ts
+++ projects/swimlane/ngx-ui/src/lib/components/select/select-helper.ts
@@ -1,4 +1,4 @@
-export function containsFilter(value, keyword, depth = 0): boolean {
+export function containsFilter(value, keyword, options?, depth = 0): boolean {
   if (value === undefined || value === null) return false;
   if (depth > 2) return false;
 
@@ -6,12 +6,15 @@ export function containsFilter(value, keyword, depth = 0): boolean {
 
   if (type === 'string') {
     if (!isNaN(value)) return value === keyword;
-    return value.indexOf(keyword) > -1;
+    const idx = options.caseSensitive ?
+      value.indexOf(keyword) :
+      value.search(new RegExp(keyword, 'i'));
+    return idx > -1;
   } else if (type === 'object') {
     const keys = Object.keys(value);
 
     for (const k of keys) {
-      if (containsFilter(value[k], keyword, depth + 1)) {
+      if (containsFilter(value[k], keyword, options, depth + 1)) {
         return true;
       }
     }
