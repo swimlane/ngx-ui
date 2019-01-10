b02cc1c194c4bbde20893feacd62afd7a75bb657
Jayson Harshbarger <hypercubed@gmail.com>
2019-01-10 09:46:52 -0700

    Fix typing issues in amTimeZone pipe

--- projects/swimlane/ngx-ui/src/lib/pipes/timezone.pipe.ts
+++ projects/swimlane/ngx-ui/src/lib/pipes/timezone.pipe.ts
@@ -1,9 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment-timezone';
 
- @Pipe({ name: 'amTimeZone' })
+const momentConstructor = moment;
+
+@Pipe({ name: 'amTimeZone' })
 export class TimeZonePipe implements PipeTransform {
-  transform(value: string, timezone: string): moment.Moment {
-    return moment(value).tz(timezone);
+  transform(value: Date | moment.Moment | string | number, timezone: string): moment.Moment {
+    return momentConstructor(value).tz(timezone);
   }
 }
