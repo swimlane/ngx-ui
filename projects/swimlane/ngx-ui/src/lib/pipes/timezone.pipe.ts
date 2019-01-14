4c6c95efa3b7fa416112277f76a25fa30df39c64
Jayson Harshbarger <hypercubed@gmail.com>
2019-01-14 16:51:04 -0700

    Fix amTimeZone for unedfined values

--- projects/swimlane/ngx-ui/src/lib/pipes/timezone.pipe.ts
+++ projects/swimlane/ngx-ui/src/lib/pipes/timezone.pipe.ts
@@ -5,7 +5,8 @@ const momentConstructor = moment;
 
 @Pipe({ name: 'amTimeZone' })
 export class TimeZonePipe implements PipeTransform {
-  transform(value: Date | moment.Moment | string | number, timezone: string): moment.Moment {
+  transform(value: Date | moment.Moment | string | number, timezone: string): moment.Moment | string {
+    if (!value) { return ''; }
     return momentConstructor(value).tz(timezone);
   }
 }
