22fec3aa398259149b7f9bf3dc7e28fd5e4e8630
Marjan Georgiev <marjan.georgiev@gmail.com>
2019-01-24 00:47:29 +0100

    Export pipes from public_api

--- projects/swimlane/ngx-ui/src/public_api.ts
+++ projects/swimlane/ngx-ui/src/public_api.ts
@@ -7,4 +7,5 @@ export * from './lib/components';
 export * from './lib/utils';
 export * from './lib/directives';
 export * from './lib/services';
+export * from './lib/pipes';
 export * from './lib/animations/animations';
