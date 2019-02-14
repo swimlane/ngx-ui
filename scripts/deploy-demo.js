a96bdd253427a8a4049a439e2db687b75880c1c4
Marjan Georgiev <marjan.georgiev@gmail.com>
2019-02-14 23:04:01 +0100

    Update base before deploying

--- scripts/deploy-demo.js
+++ scripts/deploy-demo.js
@@ -1,7 +1,26 @@
 var path = require('path');
 var ghpages = require('gh-pages');
+var fs = require('fs');
 
-var dir = path.resolve(path.join(__dirname, '../', 'dist', 'ngx-ui'));
+const distPath = path.join(__dirname, '../', 'dist', 'ngx-ui');
+var dir = path.resolve(distPath);
+
+const indexFile = `${distPath}/index.html`;
+fs.readFile(indexFile, 'utf8', function(err, data) {
+  if (err) {
+    console.log('ERROR', err);
+    return;
+  }
+  var result = data.replace('<base href="/" />', '<base href="/ngx-ui/" />');
+
+  fs.writeFile(indexFile, result, 'utf8', function(e) {
+    if (e) {
+      console.log('ERROR', e);
+      return;
+    }
+  });
+});
+
 ghpages.publish(dir, {
   user: {
     name: 'Swimlane',
