d89a1ce0b3259e54ff2550e66b11119a0da3a7f4
Marjan Georgiev <marjan.georgiev@gmail.com>
2019-05-23 16:35:11 +0200

    Add pattern validtor (#286)

--- projects/swimlane/ngx-ui/src/lib/directives/directives.module.ts
+++ projects/swimlane/ngx-ui/src/lib/directives/directives.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DblClickCopyDirective } from './dbl-click-copy.directive';
 import { VisibilityDirective } from './visibility.directive';
 import { LongPressDirective } from './long-press.directive';
+import { PatternValidatorDirective } from './validators/pattern-validator.directive';
 
 @NgModule({
-  declarations: [VisibilityDirective, DblClickCopyDirective, LongPressDirective],
+  declarations: [VisibilityDirective, DblClickCopyDirective, LongPressDirective, PatternValidatorDirective],
   exports: [VisibilityDirective, DblClickCopyDirective, LongPressDirective],
   imports: [CommonModule]
 })
