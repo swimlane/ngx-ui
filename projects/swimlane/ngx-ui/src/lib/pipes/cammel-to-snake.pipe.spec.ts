90d8f7f27a8d9e5786a89807ecce4d788019da0e
Antonio Vazquez <antonio.vazquez@swimlane.com>
2019-10-30 17:11:33 -0300

    move node card to flex

---
+++ projects/swimlane/ngx-ui/src/lib/pipes/cammel-to-snake.pipe.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { CammelToSnakePipe } from './cammel-to-snake.pipe';
+
+describe('CammelToSnakePipe', () => {
+  let pipe: CammelToSnakePipe;
+  beforeEach(() => {
+    TestBed.configureTestingModule({ providers: [CammelToSnakePipe] });
+    pipe = TestBed.get(CammelToSnakePipe);
+  });
+  it('can load instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+  it('transforms cammelToSnakePipe to cammel_to_snake_pipe', () => {
+    const value: any = 'cammelToSnakePipe';
+    const args: string[] = [];
+    expect(pipe.transform(value)).toEqual('cammel_to_snake_pipe');
+  });
+});
--- projects/swimlane/ngx-ui/src/lib/pipes/pipes.module.ts
+++ projects/swimlane/ngx-ui/src/lib/pipes/pipes.module.ts
@@ -5,10 +5,11 @@ import { FilterPipe } from './filter.pipe';
 import { DecamelizePipe } from './decamelize.pipe';
 import { JSONTreePipe } from './json-tree.pipe';
 import { TimeZonePipe } from './timezone.pipe';
+import { CammelToSnakePipe } from './cammel-to-snake.pipe';
 
 @NgModule({
-  declarations: [FilterPipe, DecamelizePipe, JSONTreePipe, TimeZonePipe],
-  exports: [FilterPipe, DecamelizePipe, JSONTreePipe, TimeZonePipe],
+  declarations: [FilterPipe, DecamelizePipe, JSONTreePipe, TimeZonePipe, CammelToSnakePipe],
+  exports: [FilterPipe, DecamelizePipe, JSONTreePipe, TimeZonePipe, CammelToSnakePipe],
   imports: [CommonModule]
 })
 export class PipesModule {}
