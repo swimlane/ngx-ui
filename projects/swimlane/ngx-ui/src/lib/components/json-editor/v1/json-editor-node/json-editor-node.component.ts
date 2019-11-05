b4317e769c675e5fc3ed0153096109d5c6611718
Antonio Vazquez <duredhel313@gmail.com>
2019-11-05 10:03:16 -0500

    fix styles

--- projects/swimlane/ngx-ui/src/lib/components/json-editor/v1/json-editor-node/json-editor-node.component.ts
+++ projects/swimlane/ngx-ui/src/lib/components/json-editor/v1/json-editor-node/json-editor-node.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { JsonEditorNode } from '../../json-editor-node';
 
 import { DialogService } from '../../../dialog/dialog.service';
@@ -6,7 +6,8 @@ import { DialogService } from '../../../dialog/dialog.service';
 @Component({
   selector: 'ngx-json-editor-node',
   templateUrl: 'json-editor-node.component.html',
-  styleUrls: ['./json-editor-node.component.scss']
+  styleUrls: ['./json-editor-node.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class JsonEditorNodeComponent extends JsonEditorNode {
 
