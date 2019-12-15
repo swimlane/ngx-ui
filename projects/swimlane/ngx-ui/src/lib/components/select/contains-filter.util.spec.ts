c7ccd7755a594f0cef3b2be008672112dad65d11
aacebo <44811138+aacebo@users.noreply.github.com>
2019-12-15 12:07:02 -0500

    test contains-filter

---
+++ projects/swimlane/ngx-ui/src/lib/components/select/contains-filter.util.spec.ts
@@ -0,0 +1,32 @@
+import { containsFilter } from './contains-filter.util';
+
+describe('containsFilter', () => {
+  it('should be true when not case sensitive and filter exists', () => {
+    const res = containsFilter('test', 'test', { });
+    expect(res).toBeTruthy();
+  });
+
+  it('should be true when case sensitive and filter exists', () => {
+    const res = containsFilter('test', 'test', { filterCaseSensitive: true });
+    expect(res).toBeTruthy();
+  });
+
+  it('should be false of !value', () => {
+    const res = containsFilter(undefined, undefined, { });
+    expect(res).toBeFalsy();
+  });
+
+  it('should traverse object value', () => {
+    const res = containsFilter({
+      test: 'test',
+      nottest: '1'
+    }, '1', { });
+
+    expect(res).toBeTruthy();
+  });
+
+  it('should be undefined if value is not string or object', () => {
+    const res = containsFilter(1, '1', { });
+    expect(res).toBeUndefined();
+  });
+});
