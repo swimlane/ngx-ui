3bcb893a78bd436b5efb142d9384d04f4a3bc2f2
Luis <luisbe090@gmail.com>
2019-12-17 15:30:56 -0600

    add unit tests

--- projects/swimlane/ngx-ui/src/lib/components/nag/nag.component.spec.ts
+++ projects/swimlane/ngx-ui/src/lib/components/nag/nag.component.spec.ts
@@ -1,22 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA, SimpleChanges } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NO_ERRORS_SCHEMA, SimpleChanges, SimpleChange } from '@angular/core';
+
 import { NagComponent } from './nag.component';
+
 describe('NagComponent', () => {
   let component: NagComponent;
   let fixture: ComponentFixture<NagComponent>;
+  const simpleChangesStub: SimpleChanges = { watch: new SimpleChange(0, 2, true) };
+
   beforeEach(() => {
-    const simpleChangesStub = { watch: {} };
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
-      declarations: [NagComponent]
+      declarations: [NagComponent],
+      imports: [NoopAnimationsModule]
     });
     fixture = TestBed.createComponent(NagComponent);
     component = fixture.componentInstance;
+
+    fixture.autoDetectChanges();
   });
+
   it('can load instance', () => {
     expect(component).toBeTruthy();
   });
+
   it('state defaults to: closed', () => {
+    expect(component.state).toEqual('closed');
+  });
+
+  it('can set component z index', () => {
+    component.zIndex = 100;
+
+    expect(component.zIndex).toEqual(100);
+  });
+
+  it('toggle changes state and triggers stateChanged emitter', () => {
+    spyOn(component.stateChanged, 'emit');
+    expect(component.state).toEqual('closed');
+
+    component.toggle();
+    expect(component.state).toEqual('open');
+
+    component.toggle();
+    expect(component.state).toEqual('closed');
+
+    expect(component.stateChanged.emit).toHaveBeenCalledTimes(2);
+  });
+
+  it('can peek into component if watch is set', () => {
+    expect(component.state).toEqual('closed');
+
+    component.ngOnChanges(simpleChangesStub);
+    expect(component.state).toEqual('peek');
+  });
+
+  it('nag remains closed if watch is falsy', () => {
+    expect(component.state).toEqual('closed');
+
+    simpleChangesStub.watch = undefined;
+    component.ngOnChanges(simpleChangesStub);
     expect(component.state).toEqual('closed');
   });
 });
