22e1ddf3a089d027a5236c5d75284805cfed969e
Luis <luisbe090@gmail.com>
2019-12-18 11:07:44 -0600

    update to OnPush and type coercion

--- projects/swimlane/ngx-ui/src/lib/components/overlay/overlay.component.ts
+++ projects/swimlane/ngx-ui/src/lib/components/overlay/overlay.component.ts
@@ -1,17 +1,16 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { coerceBooleanProperty, coerceNumberProperty } from '@angular/cdk/coercion';
 
 /**
  * Overlay Component for Drawer/Dialogs
  */
 @Component({
   selector: 'ngx-overlay',
-  template: `
-    <div (click)="click.emit(true)" [style.zIndex]="zIndex" [@overlayTransition]="animationState" class="ngx-overlay">
-      <ng-content></ng-content>
-    </div>
-  `,
+  exportAs: 'ngxOverlay',
+  templateUrl: './overlay.component.html',
   encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./overlay.component.scss'],
   animations: [
     trigger('overlayTransition', [
@@ -43,10 +42,26 @@ import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angu
   ]
 })
 export class OverlayComponent {
-  @Input() visible: boolean = false;
-  @Input() zIndex: number = 990;
+  get visible() {
+    return this._visible;
+  }
+  @Input()
+  set visible(val: boolean) {
+    this._visible = coerceBooleanProperty(val);
+  }
+
+  get zIndex() {
+    return this._zIndex;
+  }
+  @Input()
+  set zIndex(val: number) {
+    this._zIndex = coerceNumberProperty(val);
+  }
 
   @Output() click = new EventEmitter();
+
+  private _visible: boolean = false;
+  private _zIndex: number = 990;
 
   get animationState(): string {
     return this.visible ? 'active' : 'inactive';
