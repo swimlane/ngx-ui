13de18c8e0084f9667b3a0366ad48f55a2329623
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2019-12-18 10:26:56 -0500

    change to on-push and coerce input (#346)

--- projects/swimlane/ngx-ui/src/lib/components/nav-menu/nav-menu.component.spec.ts
+++ projects/swimlane/ngx-ui/src/lib/components/nav-menu/nav-menu.component.spec.ts
@@ -1,20 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 import { NavMenuComponent } from './nav-menu.component';
+
 describe('NavMenuComponent', () => {
   let component: NavMenuComponent;
   let fixture: ComponentFixture<NavMenuComponent>;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [NavMenuComponent]
     });
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(NavMenuComponent);
     component = fixture.componentInstance;
+    component.expanded = false;
+    fixture.detectChanges();
   });
+
   it('can load instance', () => {
     expect(component).toBeTruthy();
   });
+
   it('expanded defaults to: false', () => {
     expect(component.expanded).toEqual(false);
   });
