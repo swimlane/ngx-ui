c78f9062c1b27c66cddd7f0582d8014e0a77835d
Antonio Vazquez <duredhel313@gmail.com>
2020-01-05 15:21:01 -0500

    remove unused import

--- projects/swimlane/ngx-ui/src/lib/components/json-editor/json-editor.helper.ts
+++ projects/swimlane/ngx-ui/src/lib/components/json-editor/json-editor.helper.ts
@@ -1,4 +1,4 @@
-import { JSONSchema7, JSONSchema7TypeName } from 'json-schema';
+import { JSONSchema7 } from 'json-schema';
 
 export const requiredIndicatorIcon = `
   <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
