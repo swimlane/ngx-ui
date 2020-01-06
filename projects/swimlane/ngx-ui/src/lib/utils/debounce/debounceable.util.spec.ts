3e46ae7bf7158bfe55e8eaa457dd3ec33e494955
aacebo <44811138+aacebo@users.noreply.github.com>
2020-01-06 11:07:32 -0500

    add types and test debounce util

---
+++ projects/swimlane/ngx-ui/src/lib/utils/debounce/debounceable.util.spec.ts
@@ -0,0 +1,19 @@
+import { DebounceableFixture } from './debounceable.fixture';
+
+describe('debounceable', () => {
+  let fixture: DebounceableFixture;
+
+  beforeEach(() => {
+    fixture = new DebounceableFixture();
+  });
+
+  it('should call debounceable fn', (done) => {
+    const spy = spyOn(fixture, 'doSomething');
+    fixture.fn();
+
+    setTimeout(() => {
+      expect(spy).toHaveBeenCalled();
+      done();
+    }, 10);
+  });
+});
