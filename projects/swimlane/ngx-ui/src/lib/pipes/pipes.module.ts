18eaf0d79693e0e0b6808bfc459661b21f1cf5ee
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2020-01-08 11:07:56 -0500

    Aacebo/pipes  cleanup (#357)
    * update decamelize
    
    * cleanup filter util
    
    * Update pipes.module.ts
    
    * cleanup json-tree pipe
    
    * cleanup timezone pipe
    
    * run prettier

--- projects/swimlane/ngx-ui/src/lib/pipes/pipes.module.ts
+++ projects/swimlane/ngx-ui/src/lib/pipes/pipes.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FilterPipe } from './filter.pipe';
-import { DecamelizePipe } from './decamelize.pipe';
-import { JSONTreePipe } from './json-tree.pipe';
-import { TimeZonePipe } from './timezone.pipe';
+import { FilterModule } from './filter/filter.module';
+import { DecamelizeModule } from './decamelize/decamelize.module';
+import { JSONTreeModule } from './json-tree/json-tree.module';
+import { TimeZoneModule } from './time-zone/time-zone.module';
 
 @NgModule({
-  declarations: [FilterPipe, DecamelizePipe, JSONTreePipe, TimeZonePipe],
-  exports: [FilterPipe, DecamelizePipe, JSONTreePipe, TimeZonePipe],
-  imports: [CommonModule]
+  exports: [TimeZoneModule, DecamelizeModule, FilterModule, JSONTreeModule],
+  imports: [CommonModule, TimeZoneModule, DecamelizeModule, FilterModule, JSONTreeModule]
 })
 export class PipesModule {}
--- projects/swimlane/ngx-ui/src/lib/utils/index.ts
+++ projects/swimlane/ngx-ui/src/lib/utils/index.ts
@@ -2,3 +2,6 @@ export * from './debounce';
 export * from './throttle';
 export * from './id';
 export * from './position';
+export * from './is-number';
+export * from './filter';
+export * from './get-type';
