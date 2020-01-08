2c9cbc6035588c8838d184f82b12f38daba0cbb5
aacebo <44811138+aacebo@users.noreply.github.com>
2020-01-08 10:28:27 -0500

    cleanup validators

--- projects/swimlane/ngx-ui/src/lib/directives/directives.module.ts
+++ projects/swimlane/ngx-ui/src/lib/directives/directives.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DblClickCopyModule } from './dbl-click-copy/dbl-click-copy.module';
 import { VisibilityModule } from './visibility/visibility.module';
 import { LongPressModule } from './long-press/long-press.module';
-import { PatternValidatorDirective } from './validators/pattern-validator.directive';
+
+import * as validators from './validators';
 
 @NgModule({
-  declarations: [PatternValidatorDirective],
-  exports: [VisibilityModule, DblClickCopyModule, LongPressModule],
-  imports: [CommonModule, VisibilityModule, DblClickCopyModule, LongPressModule]
+  exports: [VisibilityModule, DblClickCopyModule, LongPressModule, validators.PatternValidatorModule],
+  imports: [CommonModule, VisibilityModule, DblClickCopyModule, LongPressModule, validators.PatternValidatorModule]
 })
 export class DirectivesModule {}
