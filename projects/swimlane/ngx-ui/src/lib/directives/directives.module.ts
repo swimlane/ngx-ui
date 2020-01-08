32d7a6d12c8cb767758e6624af5dc07ea0092b49
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2020-01-08 11:36:57 -0500

    Aacebo/directives  cleanup (#358)
    * update decamelize
    
    * cleanup filter util
    
    * Update pipes.module.ts
    
    * cleanup json-tree pipe
    
    * cleanup timezone pipe
    
    * run prettier
    
    * cleanup dbl-click directive
    
    * cleanup long-press directive
    
    * cleanup visibility directive
    
    * cleanup validators
    
    * run prettier
    
    Co-authored-by: Luis Bello <luisbe090@gmail.com>

--- projects/swimlane/ngx-ui/src/lib/directives/directives.module.ts
+++ projects/swimlane/ngx-ui/src/lib/directives/directives.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DblClickCopyDirective } from './dbl-click-copy.directive';
-import { VisibilityDirective } from './visibility.directive';
-import { LongPressDirective } from './long-press.directive';
-import { PatternValidatorDirective } from './validators/pattern-validator.directive';
+import { DblClickCopyModule } from './dbl-click-copy/dbl-click-copy.module';
+import { VisibilityModule } from './visibility/visibility.module';
+import { LongPressModule } from './long-press/long-press.module';
+
+import * as validators from './validators';
 
 @NgModule({
-  declarations: [VisibilityDirective, DblClickCopyDirective, LongPressDirective, PatternValidatorDirective],
-  exports: [VisibilityDirective, DblClickCopyDirective, LongPressDirective],
-  imports: [CommonModule]
+  exports: [VisibilityModule, DblClickCopyModule, LongPressModule, validators.PatternValidatorModule],
+  imports: [CommonModule, VisibilityModule, DblClickCopyModule, LongPressModule, validators.PatternValidatorModule]
 })
 export class DirectivesModule {}
---
+++ projects/swimlane/ngx-ui/src/lib/directives/validators/pattern-validator/index.ts
@@ -0,0 +1,3 @@
+export { PatternValidatorDirective } from './pattern-validator.directive';
+export { PatternValidatorModule } from './pattern-validator.module';
+export { patternValidator } from './pattern.validator';
