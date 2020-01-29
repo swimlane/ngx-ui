594bc6181ea848b070dabe4043ea42e9f26ba2bd
Luis <luisbe090@gmail.com>
2020-01-29 10:52:03 -0600

    create demo page for stepper

---
+++ src/app/components/stepper-page/stepper-page.component.ts
@@ -0,0 +1,8 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-stepper-page',
+  templateUrl: './stepper-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class StepperPageComponent {}
