277eec6c97468dafe3ca67d5643d5dfcab05ec01
aacebo <44811138+aacebo@users.noreply.github.com>
2020-01-29 21:17:31 -0500

    coerce inputs and make style responsive

--- projects/swimlane/ngx-ui/src/lib/components/stepper/stepper.component.ts
+++ projects/swimlane/ngx-ui/src/lib/components/stepper/stepper.component.ts
@@ -1,16 +1,39 @@
-import { Component, ChangeDetectionStrategy, ViewEncapsulation, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewEncapsulation, ChangeDetectorRef, Input, Output, EventEmitter } from '@angular/core';
+import { coerceNumberProperty, coerceBooleanProperty } from '@angular/cdk/coercion';
 
 @Component({
+  exportAs: 'ngxStepper',
   selector: 'ngx-stepper',
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.scss'],
+  host: { class: 'ngx-stepper' },
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
 export class StepperComponent {
-  @Input() steps: number = 0;
+  @Input()
+  get steps() { return this._steps; }
+  set steps(v: number) {
+    this._steps = coerceNumberProperty(v);
+
+    if (!isNaN(this._steps)) {
+      this.stepsArr = Array.from(Array(this._steps), (_, i) => i);
+    }
+  }
+
+  @Input()
+  get allowJumpTo() { return this._allowJumpTo; }
+  set allowJumpTo(v: boolean) {
+    this._allowJumpTo = coerceBooleanProperty(v);
+  }
+
+  @Output() indexChange = new EventEmitter<number>();
+
   stepsArr: number[] = [];
-  private _currentStep = 0;
+
+  private _currentStep: number = 0;
+  private _steps: number = 0;
+  private _allowJumpTo: boolean = false;
 
   get currentStep() {
     return this._currentStep;
@@ -18,29 +41,26 @@ export class StepperComponent {
 
   constructor(private readonly cdr: ChangeDetectorRef) {}
 
-  ngOnInit() {
-    if (!isNaN(this.steps)) {
-      this.stepsArr = Array.from(Array(this.steps), (_x, i) => i);
-    }
-  }
-
-  previousStep() {
+  previous() {
     if (this._currentStep > 0) {
       this._currentStep--;
+      this.indexChange.emit(this._currentStep);
       this.cdr.markForCheck();
     }
   }
 
-  nextStep() {
+  next() {
     if (this.stepsArr.length && this.currentStep < this.stepsArr.length - 1) {
       this._currentStep++;
+      this.indexChange.emit(this._currentStep);
       this.cdr.markForCheck();
     }
   }
 
-  jumpToStep(step: number) {
+  at(step: number) {
     if (step !== undefined && !isNaN(step) && step >= 0 && step < this.stepsArr.length) {
       this._currentStep = step;
+      this.indexChange.emit(this._currentStep);
       this.cdr.markForCheck();
     }
   }
