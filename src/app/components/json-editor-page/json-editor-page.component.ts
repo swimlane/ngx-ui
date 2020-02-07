b48d3d61409e8ca326803d89de4068a22d5ebc96
Antonio Vazquez <duredhel313@gmail.com>
2020-02-07 15:03:33 -0500

    fix undefined properties bug

--- src/app/components/json-editor-page/json-editor-page.component.ts
+++ src/app/components/json-editor-page/json-editor-page.component.ts
@@ -8,81 +8,9 @@ import { JSONSchema7 } from 'json-schema';
 })
 export class JsonEditorPageComponent {
   jsonEditorSchema = {
-    $schema: 'http://json-schema.org/draft-07/schema#',
     title: 'Product',
     description: "A product from Acme's catalog",
-    type: 'object',
-    properties: {
-      metaData: {
-        type: ['string', 'string=code', 'number', 'object']
-      },
-      productId: {
-        description: 'The unique identifier for a product',
-        type: 'number'
-      },
-      productName: {
-        description: 'Name of the product',
-        type: 'string',
-        examples: ['Apples', 'Oranges']
-      },
-      price: {
-        description: 'The price of the product',
-        type: 'number',
-        exclusiveMinimum: 0
-      },
-      tags: {
-        description: 'Tags for the product',
-        type: 'array',
-        items: {
-          type: 'string'
-        },
-        minItems: 1,
-        uniqueItems: true
-      },
-      availability: {
-        type: 'string',
-        enum: ['In Stock', 'Sold Out'],
-        default: 'In Stock'
-      },
-      onSale: {
-        description: 'The sale status of the product',
-        type: 'boolean'
-      },
-      dimensions: {
-        type: 'object',
-        properties: {
-          length: {
-            type: 'integer'
-          },
-          width: {
-            type: 'number'
-          },
-          height: {
-            type: 'number'
-          }
-        },
-        required: ['length', 'width', 'height']
-      },
-      warehouseLocation: {
-        description: 'Coordinates of the warehouse where the product is located.',
-        title: 'Longitude and Latitude',
-        required: ['latitude', 'longitude'],
-        type: 'object',
-        properties: {
-          latitude: {
-            type: 'number',
-            minimum: -90,
-            maximum: 90
-          },
-          longitude: {
-            type: 'number',
-            minimum: -180,
-            maximum: 180
-          }
-        }
-      }
-    },
-    required: ['productId', 'productName', 'price', 'availability', 'onSale', 'dimensions']
+    type: 'object'
   };
 
   compressed = false;
