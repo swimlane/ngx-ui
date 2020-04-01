05eefc8cad4df6cc67fd0274293dde8c1dbb4ab2
aacebo <44811138+aacebo@users.noreply.github.com>
2020-04-01 11:58:51 -0400

    add httpclient to icon module

--- projects/swimlane/ngx-ui/src/lib/components/icon/icon.module.ts
+++ projects/swimlane/ngx-ui/src/lib/components/icon/icon.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IconComponent } from './icon.component';
+import { IconRegistryService } from '../../services/icon-registry/icon-registry.service';
 
 @NgModule({
   declarations: [IconComponent],
   exports: [IconComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, HttpClientModule],
+  providers: [IconRegistryService]
 })
 export class IconModule {}
