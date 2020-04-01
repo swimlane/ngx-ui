be400e650b9c1a386770751f7eed660c88297662
aacebo <44811138+aacebo@users.noreply.github.com>
2020-03-31 22:26:35 -0400

    export animations

--- projects/swimlane/ngx-ui/src/public_api.ts
+++ projects/swimlane/ngx-ui/src/public_api.ts
@@ -1,6 +1,18 @@
 /*
  * Public API Surface of ngx-ui
  */
+
+// animations
+export * from './lib/animations/bounce.animation';
+export * from './lib/animations/fade-in.animation';
+export * from './lib/animations/fade-out.animation';
+export * from './lib/animations/slide-bottom.animation';
+export * from './lib/animations/slide-down-fade-out.animation';
+export * from './lib/animations/slide-left.animation';
+export * from './lib/animations/slide-right.animation';
+export * from './lib/animations/slide-top.animation';
+export * from './lib/animations/slide-up-fade-out.animation';
+
 export * from './lib/services';
 export * from './lib/components';
 export * from './lib/directives';
