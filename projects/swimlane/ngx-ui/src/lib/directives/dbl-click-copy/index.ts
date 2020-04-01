ac59da17050fab6c77d5af7a64e113f3028f28f0
aacebo <44811138+aacebo@users.noreply.github.com>
2020-03-31 22:44:24 -0400

    remove directives

--- projects/swimlane/ngx-ui/src/lib/directives/dbl-click-copy/index.ts
+++
@@ -1,2 +0,0 @@
-export { DblClickCopyDirective } from './dbl-click-copy.directive';
-export { DblClickCopyModule } from './dbl-click-copy.module';
--- projects/swimlane/ngx-ui/src/public_api.ts
+++ projects/swimlane/ngx-ui/src/public_api.ts
@@ -13,6 +13,32 @@ export * from './lib/animations/slide-right.animation';
 export * from './lib/animations/slide-top.animation';
 export * from './lib/animations/slide-up-fade-out.animation';
 
+// services
+export * from './lib/services/icon-registry/icon-registry.service';
+
+// pipes
+export * from './lib/pipes/cammel-to-snake/cammel-to-snake.module';
+export * from './lib/pipes/cammel-to-snake/cammel-to-snake.pipe';
+export * from './lib/pipes/decamelize/decamelize.module';
+export * from './lib/pipes/decamelize/decamelize.pipe';
+export * from './lib/pipes/filter/filter.module';
+export * from './lib/pipes/filter/filter.pipe';
+export * from './lib/pipes/json-tree/json-tree.module';
+export * from './lib/pipes/json-tree/json-tree.pipe';
+export * from './lib/pipes/time-zone/time-zone.module';
+export * from './lib/pipes/time-zone/time-zone.pipe';
+
+// directives
+export * from './lib/directives/dbl-click-copy/dbl-click-copy.module';
+export * from './lib/directives/dbl-click-copy/dbl-click-copy.directive';
+export * from './lib/directives/long-press/long-press.module';
+export * from './lib/directives/long-press/long-press.directive';
+export * from './lib/directives/resize-observer/resize-observer.module';
+export * from './lib/directives/resize-observer/resize-observer.directive';
+export * from './lib/directives/validators/pattern-validator/pattern-validator.module';
+export * from './lib/directives/validators/pattern-validator/pattern-validator.directive';
+export * from './lib/directives/visibility/visibility.module';
+export * from './lib/directives/visibility/visibility.directive';
+
 export * from './lib/components';
-export * from './lib/directives';
 export * from './lib/ngx-ui.module';
