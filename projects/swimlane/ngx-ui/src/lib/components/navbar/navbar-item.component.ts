0010d6e9e19eae242f82997657044af4373a80e1
Luis Bello <luisbe090@gmail.com>
2020-05-05 09:50:27 -0500

    Luisb90/ngx navbar (#437)
    * port navbar component from platform into ngx-ui
    
    * scaffold navbar demo page
    
    * update navbar animations WIP
    
    * add bar animation to navbar
    
    * removew barrel export
    
    * add unit tests
    
    * add routing example to demo
    
    * run prettier
    
    * pr feedback updates
    
    * better centering of nav items
    
    * add padding to account for ngx-icon extra height
    
    * undo prettier formatting for codemirror code bits

---
+++ projects/swimlane/ngx-ui/src/lib/components/navbar/navbar-item.component.ts
@@ -0,0 +1,72 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewEncapsulation
+} from '@angular/core';
+import { coerceNumberProperty } from '@angular/cdk/coercion';
+
+@Component({
+  exportAs: 'ngxNavbarItem',
+  selector: 'ngx-navbar-item',
+  template: ` <ng-content></ng-content> `,
+  styleUrls: ['./navbar-item.component.scss'],
+  host: {
+    class: 'ngx-navbar-item',
+    '[class.active]': 'active === index',
+    '(click)': 'setActive()'
+  },
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavbarItemComponent {
+  @Input()
+  get active() {
+    return this._active;
+  }
+  set active(v: number) {
+    if (v !== this.active) {
+      this._active = coerceNumberProperty(v);
+      this.activeChange.emit(this._active);
+      this._cdr.markForCheck();
+    }
+  }
+
+  @Input()
+  get total() {
+    return this._total;
+  }
+  set total(v: number) {
+    this._total = coerceNumberProperty(v);
+    this._cdr.markForCheck();
+  }
+
+  @Input()
+  get index() {
+    return this._index;
+  }
+  set index(v: number) {
+    this._index = coerceNumberProperty(v);
+    this._cdr.markForCheck();
+  }
+
+  @Output() activeChange = new EventEmitter<number>();
+
+  get width() {
+    return this._el.nativeElement.clientWidth;
+  }
+
+  private _active?: number;
+  private _total?: number;
+  private _index?: number;
+
+  constructor(private readonly _cdr: ChangeDetectorRef, private readonly _el: ElementRef<HTMLElement>) {}
+
+  setActive() {
+    this.active = this.index;
+  }
+}
