ea9fa9de0b1641c31193f9e5180fa90689e7a6b6
Nelson Gutierrez <nelson.gutierrez@swimlane.com>
2020-06-02 10:35:33 -0600

    Move UpdatefilterQueryIsInOptions to method on keyUp event and add debounce

--- projects/swimlane/ngx-ui/src/lib/components/select/select-dropdown.component.ts
+++ projects/swimlane/ngx-ui/src/lib/components/select/select-dropdown.component.ts
@@ -7,13 +7,15 @@ import {
   AfterViewInit,
   ElementRef,
   TemplateRef,
-  ChangeDetectionStrategy
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
 } from '@angular/core';
 import { coerceBooleanProperty, coerceNumberProperty } from '@angular/cdk/coercion';
 
 import { KeyboardKeys } from '../../enums/keyboard-keys.enum';
 import { containsFilter } from './contains-filter.util';
 import { SelectDropdownOption } from './select-dropdown-option.interface';
+import { debounceable } from '@swimlane/ngx-ui/decorators/debounceable/debounceable.decorator';
 
 @Component({
   exportAs: 'ngxSelectDropdown',
@@ -116,10 +118,6 @@ export class SelectDropdownComponent implements AfterViewInit {
     return !(typeof this.allowAdditionsText === 'object' && this.allowAdditionsText instanceof TemplateRef);
   }
 
-  get filterQueryIsInOptions() {
-    return this.options.some(o => o.name === this.filterQuery);
-  }
-
   groups: any[];
 
   private _options: SelectDropdownOption[];
@@ -130,8 +128,9 @@ export class SelectDropdownComponent implements AfterViewInit {
   private _allowAdditions: boolean;
   private _filterable: boolean;
   private _filterCaseSensitive = false;
+  filterQueryIsInOptions: boolean = false;
 
-  constructor(private readonly elementRef: ElementRef) {}
+  constructor(private readonly elementRef: ElementRef, private readonly cdr: ChangeDetectorRef) {}
 
   ngAfterViewInit(): void {
     if (this.filterable && !this.tagging) {
@@ -152,6 +151,12 @@ export class SelectDropdownComponent implements AfterViewInit {
     return idx > -1;
   }
 
+  @debounceable(500)
+  updatefilterQueryIsInOptions() {
+    this.filterQueryIsInOptions = this.options.some(o => o.name === this.filterQuery);
+    this.cdr.markForCheck();
+  }
+
   onInputKeyUp(event: KeyboardEvent): void {
     event.preventDefault();
     event.stopPropagation();
@@ -169,6 +174,7 @@ export class SelectDropdownComponent implements AfterViewInit {
       this.filterQuery = value;
     }
 
+    this.updatefilterQueryIsInOptions();
     this.keyup.emit({ event, value });
   }
 
