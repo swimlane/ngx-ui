2a59d9ad5543c4c5206db76b62ff1051d0baba63
Chau Tran <nartc7789@gmail.com>
2020-11-06 12:04:54 -0600

    feat: add withMargin prop to Input and DateTime
    - withMargin is default to `true` to keep the current behavior intact
    - withMargin with a `false` value will add `marginless` CSS class to
    `ngx-input` and `ngx-date-time` elements

--- projects/swimlane/ngx-ui/src/lib/components/date-time/date-time.component.ts
+++ projects/swimlane/ngx-ui/src/lib/components/date-time/date-time.component.ts
@@ -49,7 +49,8 @@ const DATE_TIME_VALUE_ACCESSOR = {
     '[class.sm]': 'size === "sm"',
     '[class.md]': 'size === "md"',
     '[class.lg]': 'size === "lg"',
-    '[class.autosize]': 'autosize'
+    '[class.autosize]': 'autosize',
+    '[class.marginless]': '!withMargin'
   }
 })
 export class DateTimeComponent implements OnDestroy, ControlValueAccessor {
@@ -60,6 +61,7 @@ export class DateTimeComponent implements OnDestroy, ControlValueAccessor {
   @Input() placeholder: string = '';
   @Input() size: Size = Size.Small;
   @Input() appearance: Appearance = Appearance.Legacy;
+  @Input() withMargin = true;
 
   @Input() minDate: string | Date;
   @Input() maxDate: string | Date;
