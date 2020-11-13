88918541ce31f759e2644a3def8a422ef41699eb
haeminn <haeminp@umich.edu>
2020-11-13 12:11:00 -0700

    Add card component

---
+++ projects/swimlane/ngx-ui/src/lib/components/card/card-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'ngx-card-footer',
+  exportAs: 'ngxCardFooter',
+  templateUrl: './card-footer.component.html',
+  host: {
+    class: 'ngx-card-footer'
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class CardFooterComponent {
+  @Input() label: string; // used for vertical card
+}
