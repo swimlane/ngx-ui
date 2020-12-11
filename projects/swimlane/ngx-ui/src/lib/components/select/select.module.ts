825b4b623fce8c6eab464c09db6365cdd795080b
Chau Tran <nartc7789@gmail.com>
2020-12-11 12:40:51 -0600

    feat: add custom template to group on select with groupBy (#527)
    * feat: add custom template to group on select with groupBy
    
    * change custom groupby template from a TemplateRef as content child to
    Input
    
    * fix prettier format + add groupByTemplate usage to code mirror
    
    * fix code mirror

--- projects/swimlane/ngx-ui/src/lib/components/select/select.module.ts
+++ projects/swimlane/ngx-ui/src/lib/components/select/select.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SelectDropdownComponent } from './select-dropdown.component';
+import { SelectInputComponent } from './select-input.component';
+import { SelectOptionInputTemplateDirective } from './select-option-input-template.directive';
+import { SelectOptionTemplateDirective } from './select-option-template.directive';
+import { SelectOptionDirective } from './select-option.directive';
 
 import { SelectComponent } from './select.component';
-import { SelectInputComponent } from './select-input.component';
-import { SelectDropdownComponent } from './select-dropdown.component';
-import { SelectOptionDirective } from './select-option.directive';
-import { SelectOptionTemplateDirective } from './select-option-template.directive';
-import { SelectOptionInputTemplateDirective } from './select-option-input-template.directive';
 
 @NgModule({
   declarations: [
