e916373b2a303fffcd0907806e044313675b58dc
Jayson Harshbarger <hypercubed@gmail.com>
2021-01-05 11:39:27 -0700

    Ng11 (#536)
    * ng11 update

--- projects/swimlane/ngx-ui/karma.conf.js
+++ projects/swimlane/ngx-ui/karma.conf.js
@@ -9,27 +9,16 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true,
-      skipFilesWithNoCoverage: true
-      // thresholds: {
-      //   each: {
-      //     statements: 80,
-      //     lines: 80,
-      //     branches: 80,
-      //     functions: 80
-      //   }
-      // }
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      'src/**/*.js': ['coverage']
     },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -41,6 +30,10 @@ module.exports = function (config) {
         flags: ['--no-sandbox', '--disable-gpu']
       }
     },
-    singleRun: true
+    singleRun: true,
+    coverageReporter: {
+      type: 'html',
+      dir: require('path').join(__dirname, '../../../coverage')
+    }
   });
 };
