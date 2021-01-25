c48bc7b85c5b572d6f9e4d5ab7b8fb7e848f2fd9
Caleb Ukle <caleb.ukle@MacBook-Pro.localdomain>
2021-01-25 15:36:57 -0600

    add test

--- projects/swimlane/ngx-ui/src/lib/components/date-time/date-time.component.spec.ts
+++ projects/swimlane/ngx-ui/src/lib/components/date-time/date-time.component.spec.ts
@@ -1,13 +1,13 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import moment from 'moment-timezone';
 import { MomentModule } from 'ngx-moment';
-
-import { DateTimeComponent } from './date-time.component';
-import { DialogModule } from '../dialog/dialog.module';
 import { PipesModule } from '../../pipes/pipes.module';
 import { InjectionService } from '../../services/injection/injection.service';
+import { DialogModule } from '../dialog/dialog.module';
+
+import { DateTimeComponent } from './date-time.component';
 
 (moment as any).suppressDeprecationWarnings = true;
 
@@ -26,7 +26,7 @@ const LOCAL_HOUR = LOCAL_TIME.split(':')[0];
 const LOCAL_MIN = MOON_LANDING_DATE.toLocaleTimeString('en-US', { minute: 'numeric' });
 const LOCAL_AMPM = LOCAL_TIME.slice(-2);
 
-describe('DateTimeComponent', () => {
+fdescribe('DateTimeComponent', () => {
   let component: DateTimeComponent;
   let fixture: ComponentFixture<DateTimeComponent>;
 
@@ -562,6 +562,16 @@ describe('DateTimeComponent', () => {
       const date = 'test';
       component.inputChanged(date);
       expect(component.value).toEqual(date);
+    });
+
+    it('should always emit with inputChange output', () => {
+      const invalidDate = 'abc123';
+
+      component.inputChanged(invalidDate);
+      component.inputChange.subscribe(actual => {
+        expect(actual).toEqual('abc123');
+      });
+      expect(component.value).not.toEqual(invalidDate);
     });
   });
 
