e7e4bf3a4010545999b71396f8b9c62c2bc9d9d3
Chau Tran <nartc7789@gmail.com>
2021-03-31 17:59:46 -0500

    docs(demo): add copy patch to colors page

--- packages/demo/src/app/style/colors/colors.module.ts
+++ packages/demo/src/app/style/colors/colors.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CopyToClipboardModule } from '@swimlane/ngx-ui/directives/copy-to-clipboard';
+import { CopyToClipboardPatchModule } from '../../shared/directives/copy-to-clipboard/copy-to-clipboard-patch.module';
 import { MarkdownModule } from '../../shared/ui/markdown/markdown.module';
 import { SectionHeaderModule } from '../../shared/ui/section-header/section-header.module';
 import { ColorsComponent } from './colors.component';
@@ -16,7 +17,8 @@ import { BgHexPipe } from './pipes/bg-hex.pipe';
     SectionHeaderModule,
     RouterModule.forChild([{ path: '', component: ColorsComponent }]),
     CopyToClipboardModule,
-    MarkdownModule
+    MarkdownModule,
+    CopyToClipboardPatchModule
   ],
   exports: [ColorsComponent]
 })
