8c09d78e49ddfdadc7736c264320142f8b274e88
duredhel13 <duredhel313@gmail.com>
2021-04-22 16:50:33 -0300

    feat: add viewport intersection capabilities to the dropdown menu (#572)
    * feat: add viewport intersection capabilities to the dropdown menu
    
    * feat(dropdown): include changes in changelog
    
    * refactor(dropdown): remove unused variables
    
    * fix: failing test
    
    * fix: restore the ts rule aginst unused local variables
    
    * fix: remove flash when opening
    
    * ci: fix ci node setup
    
    * ci: replace bahmutov/npm-install with official github action
    
    * chore: use bahmutov/npm-install with lock-file
    
    * chore: try bahmutov with fix version

--- projects/swimlane/ngx-ui/src/lib/components/dropdown/dropdown.module.ts
+++ projects/swimlane/ngx-ui/src/lib/components/dropdown/dropdown.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DropdownComponent } from './dropdown.component';
 import { DropdownToggleDirective } from './dropdown-toggle.directive';
 import { DropdownMenuDirective } from './dropdown-menu.directive';
+import { InViewportModule } from 'ng-in-viewport';
 
 @NgModule({
   declarations: [DropdownComponent, DropdownToggleDirective, DropdownMenuDirective],
   exports: [DropdownComponent, DropdownToggleDirective, DropdownMenuDirective],
-  imports: [CommonModule]
+  imports: [CommonModule, InViewportModule]
 })
 export class DropdownModule {}
