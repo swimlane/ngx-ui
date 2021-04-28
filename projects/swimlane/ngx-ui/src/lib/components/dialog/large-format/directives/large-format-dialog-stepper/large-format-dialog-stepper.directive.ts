531488f6ee6ccec21cd4142f98f7696c42de7cab
Chau Tran <nartc7789@gmail.com>
2021-04-28 15:16:39 -0500

    feat(dialog): change LargeFormatDialog usage

---
+++ projects/swimlane/ngx-ui/src/lib/components/dialog/large-format/directives/large-format-dialog-stepper/large-format-dialog-stepper.directive.ts
@@ -0,0 +1,6 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'ng-template[largeFormatDialogStepper]'
+})
+export class LargeFormatDialogStepperDirective {}
---
+++ projects/swimlane/ngx-ui/src/lib/components/dialog/large-format/directives/large-format-dialog-tabs/large-format-dialog-tabs.directive.ts
@@ -0,0 +1,6 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'ng-template[largeFormatDialogTabs]'
+})
+export class LargeFormatDialogTabsDirective {}
