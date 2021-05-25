3a23b80f1563d0e70a1d5f5fc47cc99a60fa4890
Mario Alvarado <74981795+mario-alva@users.noreply.github.com>
2021-05-25 08:25:26 -0600

    Add validation when change on minDate or maxDate occurs (#617)
    * Add validation when change on minDate or maxDate occurs
    
    * Fixed lint
    
    * Update change log with more explicit comment
    
    Co-authored-by: Mario Alvarado <mario.alvarado@ip-192-168-1-3.us-west-2.compute.internal>
    Co-authored-by: Mario Alvarado <mario.alvarado@ip-192-168-1-4.us-west-2.compute.internal>

--- projects/swimlane/ngx-ui/src/lib/components/date-time/date-time.component.spec.ts
+++ projects/swimlane/ngx-ui/src/lib/components/date-time/date-time.component.spec.ts
@@ -617,4 +617,30 @@ describe('DateTimeComponent', () => {
       expect(component.change.emit).not.toHaveBeenCalled();
     });
   });
+
+  describe('error messages', () => {
+    it('should update Date out of range error when minDate is changed', () => {
+      const today = new Date();
+      const yesterday = new Date();
+      yesterday.setDate(today.getDate() - 1);
+      component.minDate = today;
+      component.writeValue(yesterday);
+      expect(component.errorMsg).toEqual('Date out of range');
+      const newMinDate = new Date(yesterday.setDate(yesterday.getDate() - 1));
+      component.minDate = newMinDate;
+      expect(component.errorMsg).toEqual('');
+    });
+
+    it('should update Date out of range error when maxDate is changed', () => {
+      const today = new Date();
+      const yesterday = new Date();
+      yesterday.setDate(today.getDate() - 1);
+      component.maxDate = yesterday;
+      component.writeValue(today);
+      expect(component.errorMsg).toEqual('Date out of range');
+      const newMaxDate = new Date(today.setDate(today.getDate() + 1));
+      component.maxDate = newMaxDate;
+      expect(component.errorMsg).toEqual('');
+    });
+  });
 });
