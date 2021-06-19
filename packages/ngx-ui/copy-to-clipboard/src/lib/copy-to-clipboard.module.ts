e4e8534347cc92c762a0e201a21d277da951dc44
Chau Tran <nartc7789@gmail.com>
2021-06-19 16:43:56 -0500

    refactor(copy-to-clipboard): remove CommonModule

--- packages/ngx-ui/copy-to-clipboard/src/lib/copy-to-clipboard.module.ts
+++ packages/ngx-ui/copy-to-clipboard/src/lib/copy-to-clipboard.module.ts
@@ -1,9 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CopyToClipboardDirective } from './copy-to-clipboard.directive';
 
 @NgModule({
-  imports: [CommonModule],
   declarations: [CopyToClipboardDirective],
   exports: [CopyToClipboardDirective],
 })
