e8c2d70931a66cdff5438f5792b99316d9a38b99
Chau Tran <nartc7789@gmail.com>
2021-06-19 16:43:35 -0500

    refactor(autosize-input): remove CommonModule

--- packages/ngx-ui/autosize-input/src/lib/autosize-input.module.ts
+++ packages/ngx-ui/autosize-input/src/lib/autosize-input.module.ts
@@ -1,9 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AutosizeInputDirective } from './autosize-input.directive';
 
 @NgModule({
-  imports: [CommonModule],
   declarations: [AutosizeInputDirective],
   exports: [AutosizeInputDirective],
 })
