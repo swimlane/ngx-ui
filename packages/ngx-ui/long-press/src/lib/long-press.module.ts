7dd6f7ae7968ec21bc7adb29f1025714dfd78562
Chau Tran <nartc7789@gmail.com>
2021-06-19 16:44:06 -0500

    refactor(long-press): remove CommonModule

--- packages/ngx-ui/long-press/src/lib/long-press.module.ts
+++ packages/ngx-ui/long-press/src/lib/long-press.module.ts
@@ -1,9 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LongPressDirective } from './long-press.directive';
 
 @NgModule({
-  imports: [CommonModule],
   declarations: [LongPressDirective],
   exports: [LongPressDirective],
 })
