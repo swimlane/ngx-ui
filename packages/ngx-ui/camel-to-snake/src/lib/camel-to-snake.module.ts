4f69997a69fd70e29f9a61c76b08a071f6322eb5
Chau Tran <nartc7789@gmail.com>
2021-06-19 19:36:12 -0500

    fix(camel-to-snake): remove CommonModule

--- packages/ngx-ui/camel-to-snake/src/lib/camel-to-snake.module.ts
+++ packages/ngx-ui/camel-to-snake/src/lib/camel-to-snake.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { CamelToSnakePipe } from './camel-to-snake.pipe';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    CamelToSnakePipe
-  ],
-  exports: [
-    CamelToSnakePipe
-  ],
+  declarations: [CamelToSnakePipe],
+  exports: [CamelToSnakePipe],
 })
 export class CamelToSnakeModule {}
