770e554bbf5328103238f4efb2b08ebdfb1c354c
Chau Tran <nartc7789@gmail.com>
2021-06-20 20:53:57 -0500

    docs(docs): add FileButtonModule to docs

--- packages/docs/src/app/app.component.ts
+++ packages/docs/src/app/app.component.ts
@@ -6,34 +6,8 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class AppComponent {
-  buttonPromise?: Promise<unknown>;
-  promises: Record<string, Promise<unknown>> = {};
-
-  onClick(msg: string, targetId?: string) {
-    console.log('Demo app click: ', msg);
-
-    const buttonPromise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (Math.random() < 0.5) {
-          resolve('Success!');
-        } else {
-          reject('I fail you!');
-        }
-      }, 3000);
-    });
-
-    buttonPromise
-      .then(() => {
-        console.log('success');
-      })
-      .catch(() => {
-        console.log('fail');
-      });
-
-    if (targetId) {
-      this.promises[targetId] = buttonPromise;
-    } else {
-      this.buttonPromise = buttonPromise;
-    }
-  }
+  uploadOptions = {
+    url: 'https://evening-anchorage-3159.herokuapp.com/api/',
+    autoUpload: true
+  };
 }
--- packages/docs/src/app/app.module.ts
+++ packages/docs/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from '@swimlane/ngx-ui/button';
+import { FileButtonModule } from '@swimlane/ngx-ui/file-button';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     ButtonModule,
+    FileButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
