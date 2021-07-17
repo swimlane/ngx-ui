2403010c748341acdca978fcd17ec45f784e69c7
Chau Tran <nartc7789@gmail.com>
2021-07-17 13:10:48 -0500

    chore: adjust release-it config

--- .release-it.doc.js
+++ .release-it.doc.js
@@ -1,24 +1,3 @@
 const releaseItJson = require('./.release-it');
 
-module.exports = {
-  ...releaseItJson,
-  plugins: {
-    ...releaseItJson.plugins,
-    '@release-it/bumper': {
-      out: ['packages/ngx-doc/package*.json']
-    }
-  },
-  git: {
-    ...releaseItJson.git,
-    commitMessage: '(release-doc): ${version}'
-  },
-  hooks: {
-    'after:bump': 'git checkout -b release-doc/${version}',
-    'after:release': [
-      'git push origin HEAD --tags',
-      'git checkout -',
-      'git merge --no-edit -m=\'chore: release-doc/${version}\' release-doc/${version}',
-      'git push'
-    ]
-  }
-};
+module.exports = releaseItJson('doc');
--- .release-it.ui.js
+++ .release-it.ui.js
@@ -1,26 +1,3 @@
 const releaseItJson = require('./.release-it');
 
-module.exports = {
-  ...releaseItJson,
-  plugins: {
-    ...releaseItJson.plugins,
-    '@release-it/bumper': {
-      out: [
-        'packages/ngx-ui/package*.json'
-      ]
-    }
-  },
-  git: {
-    ...releaseItJson.git,
-    commitMessage: '(release-ui): ${version}'
-  },
-  hooks: {
-    'after:bump': 'git checkout -b release-ui/${version}',
-    'after:release': [
-      'git push origin HEAD --tags',
-      'git checkout -',
-      'git merge --no-edit -m=\'chore: release-ui/${version}\' release-ui/${version}',
-      'git push'
-    ]
-  }
-};
+module.exports = releaseItJson('ui');
