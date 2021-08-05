48a393083808fc147691d8adc069c662d4fa7941
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2021-08-05 09:42:42 -0600

    remove console log

--- cypress/integration/testing.spec.ts
+++ cypress/integration/testing.spec.ts
@@ -5,29 +5,29 @@ describe('ngx-ui demo', () => {
   });
 
   it('getByLabel works with within', () => {
-    cy.getByLabel('Name').first().then(console.log).should('have.attr', 'id', 'input1');
-    cy.getByLabel('Name').eq(1).then(console.log).should('have.attr', 'id', 'input2');
+    cy.getByLabel('Name').first().should('have.attr', 'id', 'input1');
+    cy.getByLabel('Name').eq(1).should('have.attr', 'id', 'input2');
 
     cy.get('#B').within(() => {
-      cy.getByLabel('Name').first().then(console.log).should('have.attr', 'id', 'input2');
+      cy.getByLabel('Name').first().should('have.attr', 'id', 'input2');
     });
   });
 
   it('getByPlaceholder works with within', () => {
-    cy.getByPlaceholder('Name').first().then(console.log).should('have.attr', 'id', 'input1');
-    cy.getByPlaceholder('Name').eq(1).then(console.log).should('have.attr', 'id', 'input2');
+    cy.getByPlaceholder('Name').first().should('have.attr', 'id', 'input1');
+    cy.getByPlaceholder('Name').eq(1).should('have.attr', 'id', 'input2');
 
     cy.get('#B').within(() => {
-      cy.getByPlaceholder('Name').first().then(console.log).should('have.attr', 'id', 'input2');
+      cy.getByPlaceholder('Name').first().should('have.attr', 'id', 'input2');
     });
   });
 
   it('getByName works with within', () => {
-    cy.getByName('Name').first().then(console.log).should('have.attr', 'id', 'input1');
-    cy.getByName('Name').eq(1).then(console.log).should('have.attr', 'id', 'input2');
+    cy.getByName('Name').first().should('have.attr', 'id', 'input1');
+    cy.getByName('Name').eq(1).should('have.attr', 'id', 'input2');
 
     cy.get('#B').within(() => {
-      cy.getByName('Name').first().then(console.log).should('have.attr', 'id', 'input2');
+      cy.getByName('Name').first().should('have.attr', 'id', 'input2');
     });
   });
 });
