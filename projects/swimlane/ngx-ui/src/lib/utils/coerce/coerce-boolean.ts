1336acc9097af6f397eb90066418dcd7ffc880af
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2021-08-12 13:52:53 -0600

    typing

--- projects/swimlane/ngx-ui/src/lib/utils/coerce/coerce-boolean.ts
+++ projects/swimlane/ngx-ui/src/lib/utils/coerce/coerce-boolean.ts
@@ -1,14 +1,15 @@
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
 export function CoerceBooleanProperty(): PropertyDecorator {
-  return function (target, propertyKey) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (target: Object, propertyKey: string | symbol): void {
     const _key = Symbol();
     target[_key] = target[propertyKey];
     Object.defineProperty(target, propertyKey, {
       get(): boolean {
         return this[_key];
       },
-      set(v: any) {
+      set(v: unknown) {
         this[_key] = coerceBooleanProperty(v);
       }
     });
--- projects/swimlane/ngx-ui/src/lib/utils/coerce/coerce-number.ts
+++ projects/swimlane/ngx-ui/src/lib/utils/coerce/coerce-number.ts
@@ -1,14 +1,15 @@
 import { coerceNumberProperty } from '@angular/cdk/coercion';
 
 export function CoerceNumberProperty(fallback?: number): PropertyDecorator {
-  return function (target, propertyKey) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (target: Object, propertyKey: string | symbol): void {
     const _key = Symbol();
     target[_key] = target[propertyKey];
     Object.defineProperty(target, propertyKey, {
       get(): number {
         return this[_key];
       },
-      set(v: any) {
+      set(v: unknown) {
         this[_key] = coerceNumberProperty(v, fallback);
       }
     });
