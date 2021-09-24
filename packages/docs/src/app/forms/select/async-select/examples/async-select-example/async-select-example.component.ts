52136b8e68cebd3170881528560e5c7f772536e2
Caleb Ukle <caleb.ukle@swimlane.com>
2021-09-24 11:46:21 -0500

    docs(select async): add select async to docs

--- packages/docs/src/app/forms/select/async-select/examples/async-select-example/async-select-example.component.ts
+++ packages/docs/src/app/forms/select/async-select/examples/async-select-example/async-select-example.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'docs-async-select-example',
@@ -6,8 +10,30 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AsyncSelectExampleComponent implements OnInit {
-  constructor() {}
+export class AsyncSelectExampleComponent {
+  asyncOptions$: Observable<SearchItem[]> | undefined;
 
-  ngOnInit(): void {}
+  selectControl = new FormControl([]);
+
+  search(query: string) {
+    this.asyncOptions$ = fromFetch(
+      `https://jsonplaceholder.typicode.com/posts?q=${query.toLowerCase()}`
+    ).pipe(
+      switchMap((response) => response.json()),
+      map((body: SearchItem[]) =>
+        body.filter((item) => {
+          item.isDisabled = item.id % 15 === 0;
+          return item.title.toLowerCase().includes(query);
+        })
+      )
+    );
+  }
+}
+
+interface SearchItem {
+  userId: string;
+  id: number;
+  title: string;
+  body: string;
+  isDisabled?: boolean;
 }
