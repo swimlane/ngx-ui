650c4b0e62ca7dbbd6d21211d81c820db67c66a2
Caleb Ukle <caleb.ukle@swimlane.com>
2021-09-24 11:45:54 -0500

    docs(select appearances): add select appearances to docs

--- packages/docs/src/app/forms/select/appearances-select/examples/fill-appearances-select-example/fill-appearances-select-example.component.ts
+++ packages/docs/src/app/forms/select/appearances-select/examples/fill-appearances-select-example/fill-appearances-select-example.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'docs-fill-appearances-select-example',
@@ -6,8 +7,12 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FillAppearancesSelectExampleComponent implements OnInit {
-  constructor() {}
+export class FillAppearancesSelectExampleComponent {
+  options = [
+    { name: 'Breach', value: 'breach' },
+    { name: 'DDOS', value: 'ddos' },
+    { name: 'Physical', value: 'physical' },
+  ];
 
-  ngOnInit(): void {}
+  selectControl = new FormControl([this.options[0].value]);
 }
