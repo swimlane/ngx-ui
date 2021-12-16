20bb1e3c77035e5d6d0d80f23798e1b3144f3b91
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2021-12-16 14:25:34 -0700

    remove enum

--- src/app/forms/inputs-page/inputs-page.component.ts
+++ src/app/forms/inputs-page/inputs-page.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Appearance } from '@swimlane/ngx-ui/mixins/appearance/appearance.enum';
 
 @Component({
   selector: 'app-inputs-page',
@@ -20,8 +19,6 @@ export class InputsPageComponent {
   passwordValue: any;
   output: any;
   patternValue = 'Has space';
-
-  Appearance = Appearance;
 
   onClick(event: any) {
     console.log({ event });
