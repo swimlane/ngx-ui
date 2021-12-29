6020a793f2cab56a2c477092739892c6db67210e
J. Harshbarger <hypercubed@gmail.com>
2021-12-29 11:41:10 -0700

    fix unit test imports

--- projects/swimlane/ngx-ui/src/lib/components/time-display/time-display.component.spec.ts
+++ projects/swimlane/ngx-ui/src/lib/components/time-display/time-display.component.spec.ts
@@ -1,5 +1,6 @@
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 import { NgxTimeDisplayComponent as TestComponent } from './time-display.component';
 import { MomentModule } from 'ngx-moment';
