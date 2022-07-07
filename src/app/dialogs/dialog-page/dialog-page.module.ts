9c83c2cfc1dc61b9bb77ade6a9b0bbfe80e8f801
Surya Pabbineedi <98421392+surya-pabbineedi@users.noreply.github.com>
2022-07-07 09:20:26 +0530

    feat(ngx-dialog): Restrict dialog close on closeOnEscape or closeOnBlur via beforeClose (#891)
    * feat: enhance dialog close behavior more controllable with beforeClose
    
    Co-authored-by: Jayson Harshbarger <jayson.harshbarger@swimlane.com>

--- src/app/dialogs/dialog-page/dialog-page.module.ts
+++ src/app/dialogs/dialog-page/dialog-page.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { DialogModule, SectionModule, TabsModule } from '@swimlane/ngx-ui';
+import { DialogModule, SectionModule, TabsModule, InputModule } from '@swimlane/ngx-ui';
 import { PrismModule } from '../../common/prism/prism.module';
 
 import { DialogPageRoutingModule } from './dialog-page-routing.module';
@@ -10,6 +10,15 @@ import { DialogPageComponent } from './dialog-page.component';
 
 @NgModule({
   declarations: [DialogPageComponent],
-  imports: [CommonModule, FormsModule, PrismModule, SectionModule, DialogModule, TabsModule, DialogPageRoutingModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    PrismModule,
+    InputModule,
+    SectionModule,
+    DialogModule,
+    TabsModule,
+    DialogPageRoutingModule
+  ]
 })
 export class DialogPageModule {}
