a6f506878f65bb9336d8153b2cab6f88d7c6ffd6
Nelson Gutierrez <nelson.gutierrez@swimlane.com>
2022-07-13 17:34:32 -0600

    Remove duplicated code

--- projects/swimlane/ngx-ui/src/lib/components/json-editor/json-editor-flat/json-editor-node-flat/node-types/object-node-flat/object-node-flat.component.ts
+++ projects/swimlane/ngx-ui/src/lib/components/json-editor/json-editor-flat/json-editor-node-flat/node-types/object-node-flat/object-node-flat.component.ts
@@ -175,7 +175,6 @@ export class ObjectNodeFlatComponent extends ObjectNode implements OnInit, OnCha
 
   deleteProperty(propName: string): void {
     if (!this.schemaBuilderMode) {
-      delete this.model[propName];
       this.schemaUpdate.emit();
       super.deleteProperty(propName);
       return;
