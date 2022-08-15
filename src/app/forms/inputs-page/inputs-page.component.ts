28c38534c739dd19ea1e8db14e61af0177f2018e
Mark Coleman <mark.coleman@swimlane.com>
2022-08-15 12:42:19 -0600

    Added inputs and outputs documentation to inputs page

--- src/app/forms/inputs-page/inputs-page.component.ts
+++ src/app/forms/inputs-page/inputs-page.component.ts
@@ -4,6 +4,7 @@ import { lorem } from 'faker';
 @Component({
   selector: 'app-inputs-page',
   templateUrl: './inputs-page.component.html',
+  styleUrls: ['./inputs-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InputsPageComponent {
