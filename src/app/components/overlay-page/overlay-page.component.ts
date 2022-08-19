e8a1aa8854c5c5f9bcce45f7753b9bfe0d4f3804
Mark Coleman <mark.coleman@swimlane.com>
2022-08-19 16:12:43 -0600

    Completed Overlay, Tip, and Card documentation

--- src/app/components/overlay-page/overlay-page.component.ts
+++ src/app/components/overlay-page/overlay-page.component.ts
@@ -3,6 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-overlay-page',
   templateUrl: './overlay-page.component.html',
+  styleUrls: ['./overlay-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OverlayPageComponent {}
+export class OverlayPageComponent {
+  scrollTo(id: string) {
+    (document.getElementById(id) as HTMLElement)?.scrollIntoView({ behavior: 'smooth' });
+  }
+}
