13c6a9fbce1e396f998e14ecc60fbc7189a0b5fc
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2022-09-15 13:13:33 -0600

    dead code

--- src/app/dialogs/dialog-medium-format-page/dialog-medium-format-page.module.ts
+++ src/app/dialogs/dialog-medium-format-page/dialog-medium-format-page.module.ts
@@ -2,22 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CardModule, DialogModule, DrawerModule, SectionModule, StepperModule, TabsModule } from '@swimlane/ngx-ui';
 import { PrismModule } from '../../common/prism/prism.module';
-// import { ChildDrawerComponent } from './components/child-drawer.component';
-// import { NestedStepperComponent } from './components/nested-stepper.component';
-// import { NestedTabsComponent } from './components/nested-tabs.component';
-// import { ParentDrawerComponent } from './components/parent-drawer.component';
 
 import { DialogMediumFormatDialogPageComponent } from './dialog-medium-format-dialog-page.component';
 import { DialogMediumFormatPageRoutingModule } from './dialog-medium-format-page-routing.module';
 
 @NgModule({
-  declarations: [
-    DialogMediumFormatDialogPageComponent
-    // ParentDrawerComponent,
-    // ChildDrawerComponent,
-    // NestedStepperComponent,
-    // NestedTabsComponent
-  ],
+  declarations: [DialogMediumFormatDialogPageComponent],
   imports: [
     CommonModule,
     DialogMediumFormatPageRoutingModule,
