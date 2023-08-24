399c5db143693f6bc3ede7ddc4741018a1bab565
hungnguyenswimlane <hung.nguyen@swimlane.com>
2023-08-23 19:14:35 -0700

    add unit tests

--- projects/swimlane/ngx-ui/src/lib/components/plus-menu/plus-menu.component.spec.ts
+++ projects/swimlane/ngx-ui/src/lib/components/plus-menu/plus-menu.component.spec.ts
@@ -19,7 +19,7 @@ const expectHtmlEquals = (actual: string, expected: string, options?: any) => {
   expect(actual).toEqual(expected, 'Expected HTML to match');
 };
 
-describe('PlusMenuComponent', () => {
+fdescribe('PlusMenuComponent', () => {
   let shallow: Shallow<PlusMenuComponent>;
 
   const upload = {
@@ -44,6 +44,68 @@ describe('PlusMenuComponent', () => {
   });
 
   describe('right', () => {
+    describe('one item, default colors', () => {
+      let rendering: Rendering<PlusMenuComponent, unknown>;
+
+      beforeEach(async () => {
+        rendering = await shallow.render({
+          bind: {
+            items: [create]
+          }
+        });
+      });
+
+      it('has correct items', () => {
+        expect(rendering.find('.ngx-plus-menu--item')).toHaveFound(1);
+        expect(rendering.find('.ngx-plus-menu--icon')).toHaveFound(1);
+      });
+
+      it('has correct (default) color', () => {
+        const icon0 = rendering.find('.ngx-plus-menu--icon-0').nativeElement;
+        const style = window.getComputedStyle(icon0);
+
+        expect(style.borderColor).toBe('rgb(159, 206, 54)');
+      });
+
+      it('starts closed', () => {
+        expect(rendering.instance.open).toBe(false);
+        expect(rendering.element.nativeElement).not.toHaveClass('open');
+      });
+
+      it('opens', fakeAsync(() => {
+        rendering.find('.ngx-plus-menu--circle-container').triggerEventHandler('click', null);
+        tick();
+        rendering.fixture.detectChanges();
+
+        expect(rendering.instance.open).toBe(true);
+        expect(rendering.element.nativeElement).toHaveClass('open');
+      }));
+
+      it('renders correct item content', () => {
+        expectHtmlEquals(
+          rendering.find('.ngx-plus-menu--item-0').nativeElement,
+          `
+          <div class="ngx-plus-menu--item ngx-plus-menu--item-0">
+            Create
+            <div class="subtitle">
+              ctrl + alt + n
+            </div>
+          </div>`
+        );
+      });
+
+      it('renders correct icons', () => {
+        expectHtmlEquals(
+          rendering.find('.ngx-plus-menu--icon-0').nativeElement,
+          `
+          <div class="ngx-plus-menu--icon ngx-plus-menu--icon-0">
+            <ngx-icon ng-reflect-font-icon="add-circle-thin">
+            </ngx-icon>
+          </div>`
+        );
+      });
+    });
+
     describe('two items, default colors', () => {
       let rendering: Rendering<PlusMenuComponent, unknown>;
 
@@ -185,6 +247,31 @@ describe('PlusMenuComponent', () => {
   });
 
   describe('bottom', () => {
+    describe('one item, defaults', () => {
+      let rendering: Rendering<PlusMenuComponent, unknown>;
+
+      beforeEach(async () => {
+        rendering = await shallow.render({
+          bind: {
+            items: [upload],
+            position: PlusMenuPosition.Bottom
+          }
+        });
+      });
+
+      it('shows menu title', () => {
+        expectHtmlEquals(
+          rendering.find('.ngx-plus-menu--menu-title').nativeElement,
+          '<span class="ngx-plus-menu--menu-title"></span>'
+        );
+      });
+
+      it('has correct items', () => {
+        expect(rendering.find('.ngx-plus-menu--item')).toHaveFound(1);
+        expect(rendering.find('.ngx-plus-menu--icon')).toHaveFound(1);
+      });
+    });
+
     describe('two items, defaults', () => {
       let rendering: Rendering<PlusMenuComponent, unknown>;
 
