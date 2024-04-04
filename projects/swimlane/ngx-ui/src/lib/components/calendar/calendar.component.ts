ef6f5a42603639d71707804d81147e8d62a4185f
Prasanth-Boyina <prasanth.boyina@swimlane.com>
2024-04-04 11:55:07 +0530

    SPT-27073: Test fix & Displaying date in input box

--- projects/swimlane/ngx-ui/src/lib/components/calendar/calendar.component.ts
+++ projects/swimlane/ngx-ui/src/lib/components/calendar/calendar.component.ts
@@ -693,6 +693,15 @@ export class CalendarComponent implements OnInit, AfterViewInit, ControlValueAcc
     this.cdr.detectChanges();
   }
 
+  formatDate(date: Date): string {
+    const months: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const month: string = months[date.getMonth()];
+    const day: number = date.getDate();
+    const year: number = date.getFullYear();
+
+    return `${month} ${day}, ${year}`;
+  }
+
   private onChangeCallback: (_: any) => void = () => {
     // placeholder
   };
