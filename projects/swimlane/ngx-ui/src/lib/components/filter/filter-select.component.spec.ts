ba946058a1a041d49ae818ad8b5f81afa2b0665e
Jonathan Avendano <jogaj_26@hotmail.com>
2025-06-24 08:25:49 -0600

    Adding type button v1

--- projects/swimlane/ngx-ui/src/lib/components/filter/filter-select.component.spec.ts
+++ projects/swimlane/ngx-ui/src/lib/components/filter/filter-select.component.spec.ts
@@ -2,6 +2,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FilterSelectComponent } from './filter-select.component';
 import { SelectDropdownOption } from '../select/select-dropdown-option.interface';
+import { FilterType } from './filter.type.enum';
 
 describe('FilterSelectComponent', () => {
   let component: FilterSelectComponent;
@@ -55,6 +56,51 @@ describe('FilterSelectComponent', () => {
       const spyOnSelectAll = spyOn(component, 'onSelectAll');
       component.onDropdownSelection(options[2] as SelectDropdownOption);
       expect(spyOnSelectAll).not.toHaveBeenCalled();
+    });
+
+    it('should return true for isSingleSelect when not multiple and filterType equals to Dropdown', () => {
+      component.type = FilterType.Dropdown;
+      component.multiple = false;
+      expect(component.isSingleSelect).toBeTruthy();
+    });
+
+    it('should return false for isSingleSelect when multiple and filterType equals to Dropdown', () => {
+      component.type = FilterType.Dropdown;
+      component.multiple = true;
+      expect(component.isSingleSelect).toBeFalsy();
+    });
+
+    it('should return false for isSingleSelect when filterType is different from Dropdown', () => {
+      component.type = FilterType.Button;
+      expect(component.isSingleSelect).toBeFalsy();
+    });
+
+    it('should return filterCount for filterType Dropdown based-on values', () => {
+      component.type = FilterType.Dropdown;
+      component.value = ['a', 'b'];
+      expect(component.filterCount).toBe(2);
+      expect(component.hasFilters).toBeTruthy();
+    });
+
+    it('should return filterCount and hasFilters for filterType Button', () => {
+      component.type = FilterType.Button;
+      component.filterCount = 55;
+      expect(component.filterCount).toBe(55);
+      expect(component.hasFilters).toBeTruthy();
+    });
+
+    it('should emit click emitter when button is enabled', () => {
+      const spyClickEmit = spyOn(component.click, 'emit');
+      component.disabled = false;
+      component.onButtonFilterClick({ stopPropagation: () => {} });
+      expect(spyClickEmit).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not emit click emitter when button is disabled', () => {
+      const spyClickEmit = spyOn(component.click, 'emit');
+      component.disabled = true;
+      component.onButtonFilterClick({ stopPropagation: () => {} });
+      expect(spyClickEmit).not.toHaveBeenCalled();
     });
   });
 });
